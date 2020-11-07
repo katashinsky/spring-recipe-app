aac00473b84a17bc843a60992423f96110c0b786
Maks <Katashinskydmitry@gmail.com>
2020-11-07 19:44:11 +0200

    add recipes app

---
+++ src/main/java/guru/springframework/domains/Ingredient.java
@@ -0,0 +1,70 @@
+package guru.springframework.domains;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unit;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure unit, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.unit = unit;
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnit() {
+        return unit;
+    }
+
+    public void setUnit(UnitOfMeasure unit) {
+        this.unit = unit;
+    }
+}
---
+++ src/main/java/guru/springframework/services/RecipeServiceImp.java
@@ -0,0 +1,26 @@
+package guru.springframework.services;
+
+import guru.springframework.domains.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImp implements RecipeService{
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImp(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipes = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipe -> {
+            recipes.add(recipe);
+        });
+        return recipes;
+    }
+}
