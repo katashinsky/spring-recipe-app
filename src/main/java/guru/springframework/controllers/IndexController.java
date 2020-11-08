18d0b892a7b2c020e7c3e999ae4afadbce8928d9
Maks <Katashinskydmitry@gmail.com>
2020-11-08 16:23:15 +0200

    add lobok

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -5,12 +5,14 @@ import guru.springframework.domains.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +24,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     String getIndexPage(Model model){
+        log.debug("In index page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
--- src/main/java/guru/springframework/services/RecipeServiceImp.java
+++ src/main/java/guru/springframework/services/RecipeServiceImp.java
@@ -2,11 +2,13 @@ package guru.springframework.services;
 
 import guru.springframework.domains.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImp implements RecipeService{
     private final RecipeRepository recipeRepository;
@@ -17,10 +19,9 @@ public class RecipeServiceImp implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in RecipeServiceImp");
         Set<Recipe> recipes = new HashSet<>();
-        recipeRepository.findAll().iterator().forEachRemaining(recipe -> {
-            recipes.add(recipe);
-        });
+        recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
     }
 }
