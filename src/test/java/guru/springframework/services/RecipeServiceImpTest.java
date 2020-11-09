df6019157c17726026f54131a80aa95368277c72
Maks <Katashinskydmitry@gmail.com>
2020-11-09 22:39:36 +0200

    add plugin and unit tests

---
+++ src/test/java/guru/springframework/services/RecipeServiceImpTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.services;
+
+import guru.springframework.domains.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RecipeServiceImpTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImp(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes(){
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
